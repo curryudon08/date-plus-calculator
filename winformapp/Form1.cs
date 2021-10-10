349bb33058f112228753d3661217248d8315d38f
udonk <6565941+curryudon08@users.noreply.github.com>
2021-10-10 18:36:10 +0900

    日付を計算する処理を実装

--- winformapp/Form1.cs
+++ winformapp/Form1.cs
@@ -22,7 +22,7 @@ namespace winformapp
         /// <summary>
         /// 画面の表示を初期化する
         /// </summary>
-        public void InitializeDisplay()
+        private void InitializeDisplay()
         {
             //入力箇所の初期化
             this.yearNowText.Text = DateTime.Now.Year.ToString();
@@ -34,13 +34,53 @@ namespace winformapp
             this.monthPlusText.Text = "1";
 
             //結果の初期化
-            this.resultDateLabel.Text = "YYYY年MM月DD日（月曜日）";
+            this.resultDateLabel.Text = "YYYY年MM月DD日（月）";
         }
 
-
-        public void execBtn_Click(object sender, EventArgs e)
+        private void execBtn_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("動作確認");
+            try{
+                var y = int.Parse(this.yearNowText.Text);
+                var m = int.Parse(this.monthNowText.Text);
+                var d = int.Parse(this.dayNowText.Text);
+                var date = new DateTime(y,m,d);
+                var afterDate = CalcDate(date);
+                this.ShowCalcResult(afterDate);
+            }catch(Exception ex){
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 計算結果を画面に表示する
+        /// </summary>
+        private void ShowCalcResult(DateTime date)
+        {
+            var strdate = date.ToString("yyyy年MM月dd日");
+            var culture = System.Globalization.CultureInfo.GetCultureInfo("ja-JP");
+            var strdayofweek = date.ToString("ddd",culture);
+            this.resultDateLabel.Text = $"{strdate}（{strdayofweek}）";
+        }
+
+        /// <summary>
+        /// 加算後の日付を計算する
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        private DateTime CalcDate(DateTime date)
+        {
+            if(this.dayPlusRadioBtn.Checked){
+                var d = int.Parse(this.dayPlusText.Text);
+                return date.AddDays(d);
+            }
+
+            if(this.weekPlusRadioBtn.Checked){
+                var d = int.Parse(this.weekPlusText.Text) * 7;
+                return date.AddDays(d);
+            }
+
+            var m = int.Parse(this.monthPlusText.Text);
+            return date.AddMonths(m);
         }
     }
 }
